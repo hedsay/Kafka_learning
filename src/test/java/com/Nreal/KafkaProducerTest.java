fe2c778ea3547067dbaa0688aa06eb3a638330c7
konvergenz <1141955640@qq.com>
2024-01-01 21:13:27 +0800

    v1.1 add kafka入门测试案例

---
+++ src/test/java/com/Nreal/KafkaProducerTest.java
@@ -0,0 +1,30 @@
+package com.Nreal;
+
+import com.Nreal.producer.KafkaProducer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaProducerTest {
+
+    private Logger logger = LoggerFactory.getLogger(KafkaProducerTest.class);
+
+    @Resource
+    private KafkaProducer kafkaProducer;
+
+    @Test
+    public void test_send() throws InterruptedException {
+        // 循环发送消息
+        while (true) {
+            kafkaProducer.send("hello,Nreal");
+            Thread.sleep(3500);
+        }
+    }
+}
